3a68e2248ac6cf475e0c99454959f5f858c2ae17
matu3980454 <131503530+matu3980454@users.noreply.github.com>
2023-06-03 21:49:40 +0900

    Add files via upload

--- flame01.py
+++ flame01.py
@@ -56,3 +56,22 @@ if uploaded_file is not None:
 df = pd.DataFrame(
      np.random.randn(10, 2) / [50, 50] + [35.51, 139.56],columns=['lat', 'lon'])
 st.map(df)
+
+
+from barfi import st_barfi, barfi_schemas
+import streamlit as st
+from test_blocks import process_blocks
+
+barfi_schema_name = st.selectbox(
+    'Select a saved schema to load:', barfi_schemas())
+
+compute_engine = st.checkbox('Activate barfi compute engine', value=False)
+
+barfi_result = st_barfi(
+    base_blocks=process_blocks, compute_engine=compute_engine, load_schema=barfi_schema_name)
+
+if barfi_result:
+    st.write(barfi_result['Result-id-524173']['block'].get_interface(name='Input 1'))
+    st.write(barfi_result)
+
+st.write("bbbbbbbbbb")
